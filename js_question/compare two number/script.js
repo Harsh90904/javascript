95b92c0e7c6c592ffa424bc62e0056a0ba59f8ae
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-06-03 10:44:05 +0530

    Delete js_question directory

--- js_question/compare two number/script.js
+++
@@ -1,9 +0,0 @@
-function comparetwo(){
-    let a = parseInt(document.getElementById('num-1').value);
-    let b = parseInt(document.getElementById('num-2').value);
-    let msg;
-
-    document.getElementById('flase').innerHTML = Boolean(a > b);
-    document.getElementById('true').innerHTML = Boolean(a < b);
-    document.getElementById('dis').innerHTML = Boolean(a == b);
-}
